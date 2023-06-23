d34564b1827e7b3e8dcd51cf7a8119cb946e7aee
chedgala <cgalarza88@gmail.com>
2023-06-23 17:58:15 -0500

    Student t fit
    Setting MCMC parameters

--- runSKT.R
+++ runSKT.R
@@ -3,7 +3,9 @@
 ############################################################################
 
 library(lqr)
-n<-200
+#n<-200
+n<-50
+
 p<-0.75
 sigmas<-1.3
 nu=3
@@ -25,31 +27,31 @@ library(shinystan)
 initf1 <- function() {list(beta=modelT$beta, sigma=modelT$sigma, u=rep(0.8,n))}
 initf1
 
+iters = 5000
+
 fit_stan <- stan(file='SKT.stan', init=  initf1,
                  data = list(y=c(y),w=c(y), x_p=x_p,x=x1,n=n,p=p),
-                 thin = 1000, chains = 1, iter = 20000, warmup = 1000,
-                 seed = 9955)
+                 chains = 1, iter = iters*1.05, warmup = iters*0.05) #seed = 9955
 
-library(rstanmulticore)
-
-initf1 <- function(n) {list(beta=c(-1.051333 ,2.129753 ,2.651769),
-                           sigma=1.384141, u=rep(0.8,200))}
-
-fit_stan <- pstan(file='SKT.stan', init=initf1,
-                 data = list(y=c(y),w=c(y), x_p=x_p,x=x1,n=n,p=p),
-                 thin = 1000, chains = 4, iter = 10000, warmup = 1000,
-                 seed = 9955,verbose = TRUE)
-
-library(microbenchmark)
-microbenchmark(stan(file='SKT.stan', init=initf1,
-                                     data = list(y=c(y),w=c(y), x_p=x_p,x=x1,n=n,p=p),
-                                     thin = 1000, chains = 4, iter = 10000, warmup = 1000,
-                                     seed = 9955,verbose = TRUE),
-                               pstan(file='SKT.stan', init=initf1,
-                                     data = list(y=c(y),w=c(y), x_p=x_p,x=x1,n=n,p=p),
-                                     thin = 1000, chains = 4, iter = 10000, warmup = 1000,
-                                     seed = 9955,verbose = TRUE),times = 5)
-
+# library(rstanmulticore)
+# 
+# initf1 <- function(n) {list(beta=c(-1.051333 ,2.129753 ,2.651769),
+#                            sigma=1.384141, u=rep(0.8,200))}
+# 
+# fit_stan <- pstan(file='SKT.stan', init=initf1,
+#                  data = list(y=c(y),w=c(y), x_p=x_p,x=x1,n=n,p=p),
+#                  thin = 1000, chains = 4, iter = 10000, warmup = 1000,
+#                  seed = 9955,verbose = TRUE)
+# 
+# library(microbenchmark)
+# microbenchmark(stan(file='SKT.stan', init=initf1,
+#                                      data = list(y=c(y),w=c(y), x_p=x_p,x=x1,n=n,p=p),
+#                                      thin = 1000, chains = 4, iter = 10000, warmup = 1000,
+#                                      seed = 9955,verbose = TRUE),
+#                                pstan(file='SKT.stan', init=initf1,
+#                                      data = list(y=c(y),w=c(y), x_p=x_p,x=x1,n=n,p=p),
+#                                      thin = 1000, chains = 4, iter = 10000, warmup = 1000,
+#                                      seed = 9955,verbose = TRUE),times = 5)
 
 #qoi <- c("beta","sigma")
 qoi <- c("beta","sigma","nu")
@@ -61,3 +63,7 @@ lqr(y~x1-1,dist = "t", p=p, precision = 0.000000001)
 
 #modelN = lqr(y~x1-1,dist = "slash", p=p, precisio=0.000000001)
 ##################################################
+
+length(rstan::extract(fit_stan)$beta[,1])
+
+plot.ts(rstan::extract(fit_stan)$beta[,1])
